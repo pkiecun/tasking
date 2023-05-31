b5d1de788bbb134cb96ae62dd3797efd7a0cdc23
unknown <pkiecun@gmail.com>
2023-05-31 14:39:16 -0500

    starter

---
+++ src/main/java/com/revature/tasking/TaskingApplication.java
@@ -0,0 +1,13 @@
+package com.revature.tasking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskingApplication.class, args);
+	}
+
+}
